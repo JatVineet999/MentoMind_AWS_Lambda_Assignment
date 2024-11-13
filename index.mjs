310773f5b220175224cddedf032f79b3f291dbc3
VINEET KUMAR PILANIA <92670607+JatVineet999@users.noreply.github.com>
2024-11-13 12:23:12 +0530

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,9 +1,7 @@
 import { CommandFactory } from "./factories/CommandFactory.js";
 
-// Lambda handler function
 export const handler = async (event, context) => {
   try {
-    // Extract data from the event object
     const {
       userId,
       topicId,
@@ -12,7 +10,6 @@ export const handler = async (event, context) => {
       numberOfHardQuestions,
     } = event;
 
-    // Initialize CommandFactory and create the quiz command
     const commandFactory = new CommandFactory();
     const command = commandFactory.createGenerateQuizCommand(
       userId,
@@ -22,19 +19,16 @@ export const handler = async (event, context) => {
       numberOfHardQuestions
     );
 
-    // Execute the command to get quiz questions (assuming the command already returns a JSON object)
     const result = await command.execute();
 
-    // Return the result as a proper JSON object (no stringifying)
     return {
       statusCode: 200,
-      body: result, // Send the result as is, without stringifying
+      body: result,
     };
   } catch (error) {
-    // Return error response in JSON format
     return {
       statusCode: 500,
-      body: { error: error.message }, // Return the error message as a JSON object
+      body: { error: error.message },
     };
   }
 };
