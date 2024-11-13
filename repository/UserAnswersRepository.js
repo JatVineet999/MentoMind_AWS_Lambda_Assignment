d43f118c206e7d5a6589fd429ad05dbafb3586ca
VINEET KUMAR PILANIA <92670607+JatVineet999@users.noreply.github.com>
2024-11-13 12:22:06 +0530

    Update UserAnswersRepository.js

--- repository/UserAnswersRepository.js
+++ repository/UserAnswersRepository.js
@@ -3,28 +3,24 @@ import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
 
 export class UserAnswersRepository {
   constructor() {
-    // Initialize DynamoDB client
     const dynamoDbClient = new DynamoDBClient({ region: "ap-south-1" });
-
-    // Wrap it in DynamoDBDocumentClient
     this.dynamoDbClient = DynamoDBDocumentClient.from(dynamoDbClient);
-    this.tableName = process.env.USER_TEST_INSTANCE_ANSWER_TABLE_NAME; // Fetch table name from environment variables
+    this.tableName = process.env.USER_TEST_INSTANCE_ANSWER_TABLE_NAME;
   }
 
-  // Fetch attempted questions by user and topic
   async getUserAttemptedQuestions(userId, topicId) {
     const params = {
       TableName: this.tableName,
       KeyConditionExpression: "userTestInstanceId = :userTestInstanceId",
       ExpressionAttributeValues: {
-        ":userTestInstanceId": `${userId}#${topicId}`, // Combine userId and topicId
+        ":userTestInstanceId": `${userId}#${topicId}`,
       },
       ProjectionExpression: "qId",
     };
 
     try {
-      const data = await this.dynamoDbClient.send(new QueryCommand(params)); // Send the QueryCommand
-      return data.Items.map((item) => item.qId); // Map to return only qId values
+      const data = await this.dynamoDbClient.send(new QueryCommand(params));
+      return data.Items.map((item) => item.qId);
     } catch (error) {
       console.error("Error fetching attempted questions:", error);
       throw new Error("Could not fetch attempted questions");
